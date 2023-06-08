6b7bbd20d3e4fe4d5fd138c17d3bd632e6d94437
SwethaReddySalkuti <salkutiswethareddy@gmail.com>
2023-06-08 17:29:12 +0530

    premium

--- controllers/premium.js
+++ controllers/premium.js
@@ -5,10 +5,24 @@ const sequelize = require('../util/database');
 exports.getUserLeaderBoard = async (req, res) => {
     try
     {
-    
+        const leaderboardofusers = await User.findAll({
+            attributes: ['id', 'name',[sequelize.fn('sum', sequelize.col('expenses.expenseamount')), 'total_cost'] ],
+            include: [
+                {
+                    model: Expense,
+                    attributes: []
+                }
+            ],
+            group:['user.id'],
+            order:[['total_cost', 'DESC']]
+
+        })
+       
+        res.status(200).json(leaderboardofusers)
     } 
-    catch (err){
+    catch (err)
+    {
     res.status(500).json(err)
-}
+    }
 }
 

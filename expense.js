bc0ec8df64fcb579f83ed486a51001604bb7f4b1
SwethaReddySalkuti <salkutiswethareddy@gmail.com>
2023-06-13 10:48:41 +0530

    download

--- expense.js
+++ expense.js
@@ -152,4 +152,20 @@ window.addEventListener("DOMContentLoaded", async () => {
     }
     document.getElementById("message").appendChild(inputElement);
   }
-  
+  function download(){
+    axios.get('http://localhost:3000/user/download', { headers: {"Authorization" : token} })
+    .then((response) => {
+        if(response.status === 201){
+            var a = document.createElement("a");
+            a.href = response.data.fileUrl;
+            a.download = 'myexpense.csv';
+            a.click();
+        } else {
+            throw new Error(response.data.message)
+        }
+
+    })
+    .catch((err) => {
+        showError(err)
+    });
+}
